162157f84b4ee24f5caba768a6dd031018754ff9
iliyasold <iliyasold@gmail.com>
2022-02-17 00:56:21 +0300

    Fixed uuid

--- src/CommentWidget/CommentList.js
+++ src/CommentWidget/CommentList.js
@@ -7,14 +7,14 @@ function CommentList(props) {
         <ul className="CommentList list">
             <h2 className="second-title CommentList-title">Все комментарии</h2>
 
-            {comments.map((comment, position) => {
+            {comments.map((comment) => {
                     return <CommentItem
                         key={comment.itemId}
                         userName={comment.userName}
                         commentText={comment.commentText}
                         currentDate={comment.currentDate}
                         currentTime={comment.currentTime}
-                        removeItem={() => props.removeCommentItem(position)}
+                        removeItem={() => props.removeCommentItem(comment.itemId)}
                     />
                 }
             )}

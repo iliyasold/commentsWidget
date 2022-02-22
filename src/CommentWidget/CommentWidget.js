815d2531a751c424fdbdce9f15071f9b91ab3c35
iliyasold <iliyasold@gmail.com>
2022-02-22 13:17:30 +0300

    Fixed func "addItem" & added func download localStorage items

--- src/CommentWidget/CommentWidget.js
+++ src/CommentWidget/CommentWidget.js
@@ -1,10 +1,20 @@
 import CommentForm from './CommentForm'
 import CommentList from './CommentList'
 import CommentatorsTop from "./CommentatorsTop";
-import {useState} from 'react';
+import React, {useState} from 'react';
 
 function CommentWidget() {
     const [items, setItems] = useState([]);
+
+    window.onload = function () {
+        const storageItems = []
+
+        for (let i = 0; i < localStorage.length; i++) {
+            storageItems.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
+        }
+
+        setItems(storageItems);
+    };
 
     function addItem(item) {
         const newItems = [...items]
@@ -13,7 +23,7 @@ function CommentWidget() {
 
         setItems(newItems);
 
-        const storageNewItems = JSON.stringify(newItems);
+        const storageNewItems = JSON.stringify(item);
 
         localStorage.setItem(item.itemId, storageNewItems);
     }
@@ -35,7 +45,7 @@ function CommentWidget() {
             <div className="CommentForm-wrap">
                 <CommentForm addNewItem={addItem}/>
 
-                <CommentList comments={items} removeCommentItem={removeCommentItem} />
+                <CommentList comments={items} removeCommentItem={removeCommentItem}/>
             </div>
 
             <CommentatorsTop names={items}/>
